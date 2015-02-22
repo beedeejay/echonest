4606e44e0b7b59117157139bb0fa1a38dc1e92f2
Greg <gregorymastoras@gmail.com>
2015-02-22 17:02:15 +0200

    Changed home.php to song.php in controllers and views.

---
+++ application/controllers/song.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Song extends CI_Controller {
+
+    public function index() {
+        $this->load->view('song');
+    }
+	
+	function __construct() {
+		$apiKey = "I6IXY87XH7YGDMEWO";
+	}
+	
+
+    public function searchSong()
+    {
+        $artist = $this->input->post('artist');
+        $title = $this->input->post('title');
+		
+		$audio_summary = $this->input->post('audio_summary');
+		$artist_discovery = $this->input->post('artist_discovery');
+		$artist_discovery_rank = $this->input->post('artist_discovery_rank');
+		$artist_familiarity = $this->input->post('artist_familiarity');
+		$artist_familiarity_rank = $this->input->post('artist_familiarity_rank');
+		$artist_hotttnesss = $this->input->post('artist_hotttnesss');
+		$artist_hotttnesss_rank = $this->input->post('artist_hotttnesss_rank');
+		$artist_location = $this->input->post('artist_location');
+		$song_currency = $this->input->post('song_currency');
+		$song_currency_rank = $this->input->post('song_currency_rank');
+		$song_discovery = $this->input->post('song_discovery');
+		$song_discovery_rank = $this->input->post('song_discovery_rank');
+		$song_hotttnesss = $this->input->post('song_hotttnesss');
+		$song_hotttnesss_rank = $this->input->post('song_hotttnesss_rank');
+		$song_type = $this->input->post('song_type');
+
+		// Construct request string
+		$title = str_replace(' ', '%20', $title);
+		$artist = str_replace(' ', '%20', $artist);
+		$request_str = 'http://developer.echonest.com/api/v4/song/search?api_key='.$apiKey.'&format=json';
+		
+		if (!is_null($artist)) $request_str = $request_str . '&artist=' . $artist;
+		if (!is_null($title)) $request_str = $request_str . '&title=' . $title;
+		
+		if ($audio_summary) $request_str = $request_str . '&bucket=' . $title;
+		if ($artist_discovery) $request_str = $request_str . '&bucket=artist_discovery';
+		if ($artist_discovery_rank) $request_str = $request_str . '&bucket=artist_discovery_rank';
+		if ($artist_familiarity) $request_str = $request_str . '&bucket=artist_familiarity';
+		if ($artist_familiarity_rank) $request_str = $request_str . '&bucket=artist_familiarity_rank';
+		if ($artist_hotttnesss) $request_str = $request_str . '&bucket=artist_hotttnesss';
+		if ($artist_hotttnesss_rank) $request_str = $request_str . '&bucket=artist_hotttnesss_rank';
+		if ($artist_location) $request_str = $request_str . '&bucket=artist_location';
+		if ($song_currency) $request_str = $request_str . '&bucket=song_currency';
+		if ($song_currency_rank) $request_str = $request_str . '&bucket=song_currency_rank';
+		if ($song_discovery) $request_str = $request_str . '&bucket=song_discovery';
+		if ($song_discovery_rank) $request_str = $request_str . '&bucket=song_discovery_rank';
+		if ($song_hotttnesss) $request_str = $request_str . '&bucket=song_hotttnesss';
+		if ($song_hotttnesss_rank) $request_str = $request_str . '&bucket=song_hotttnesss_rank';
+		if ($song_type) $request_str = $request_str . '&bucket=song_type';
+		
+
+        $curl = curl_init(request_str);
+
+        // Don't output the result
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        // Send the request
+        $result = curl_exec($curl);
+
+        // Get info about the cURL Request
+        $query_info = curl_getinfo($curl);
+
+        // Free up the resources $curl is using
+        curl_close($curl);
+
+        $data["curl_result"] = $result;
+        $data["query_info"] = $query_info;
+
+        $this->load->view('home', $data);
+    }
+}
---
+++ application/views/song.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <title>Echonest Explorer - Song API</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-3.3.2-dist/css/bootstrap.css');?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-3.3.2-dist/css/bootstrap-theme.css');?>">
+
+    <style type="text/css">
+
+        ::selection { background-color: #E13300; color: white; }
+        ::-moz-selection { background-color: #E13300; color: white; }
+        ::-webkit-selection { background-color: #E13300; color: white; }
+
+        body {
+            background-color: #fff;
+            margin: 40px;
+            font: 13px/20px normal Helvetica, Arial, sans-serif;
+            color: #4F5155;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="container" style="margin: 100px; background-color: #C0C0C0; padding: 20px;">
+        <form action="<?php echo site_url('home/getinfo'); ?> " role="form" method="post" accept-charset="utf-8">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Artist name</label>
+                <input type="text" class="form-control" id="artist" name="artist" placeholder="Artist name" value="<?php echo set_value('artist' ,''); ?>" >
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Song name</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Song name" value="<?php echo set_value('title' ,''); ?>">
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"  name="audio_summary" id="audio_summary" value="True"  <?php echo set_checkbox('audio_summary', 'True'); ?> />
+                    audio_summary</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"  id="artist_discovery" name="artist_discovery" value="True" <?php echo set_checkbox('artist_discovery', 'True'); ?> />
+                    artist_discovery</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"  id="artist_discovery_rank" name="artist_discovery_rank" value="True" <?php echo set_checkbox('artist_discovery_rank', 'True'); ?>>
+                    artist_discovery_rank</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="artist_familiarity" name="artist_familiarity"  value="True"<?php echo set_checkbox('artist_familiarity', 'True'); ?>>
+                    artist_familiarity</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox" id="artist_familiarity_rank" name="artist_familiarity_rank" value="True" <?php echo set_checkbox('artist_familiarity_rank', 'True'); ?>>
+                    artist_familiarity_rank</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="artist_hotttnesss" name="artist_hotttnesss" value="True" <?php echo set_checkbox('artist_hotttnesss', 'True'); ?>>
+                    artist_hotttnesss</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="artist_hotttnesss_rank" name="artist_hotttnesss_rank" value="True" <?php echo set_checkbox('artist_hotttnesss_rank', 'True'); ?> >
+                    artist_hotttnesss_rank</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="artist_location" name="artist_location" value="True" <?php echo set_checkbox('artist_location', 'True'); ?>>
+                    artist_location</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_currency" name="song_currency" value="True" <?php echo set_checkbox('song_currency', 'True'); ?>>
+                    song_currency</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_currency_rank" name="song_currency_rank" value="True" <?php echo set_checkbox('song_currency_rank', 'True'); ?>>
+                    song_currency_rank</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_discovery" name="song_discovery" value="True" <?php echo set_checkbox('song_discovery', 'True'); ?>>
+                    song_discovery</label>
+            </div>
+
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_discovery_rank" name="song_discovery_rank" value="True" <?php echo set_checkbox('song_discovery_rank', 'True'); ?>>
+                    song_discovery_rank</label>
+            </div>
+			
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_hotttnesss" name="song_hotttnesss" value="True" <?php echo set_checkbox('song_hotttnesss', 'True'); ?>>
+                    song_hotttnesss</label>
+            </div>
+			
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_hotttnesss_rank" name="song_hotttnesss_rank" value="True" <?php echo set_checkbox('song_hotttnesss_rank', 'True'); ?>>
+                    song_hotttnesss_rank</label>
+            </div>
+			
+            <div class="checkbox">
+                <label for="exampleInputPassword1">
+                    <input type="checkbox"   id="song_type" name="song_type" value="True" <?php echo set_checkbox('song_type', 'True'); ?>>
+                    song_type</label>
+            </div>
+			
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        </br>
+        </br>
+        <div>
+            <fieldset>
+                <legend> Http Query:</legend>
+                <?php
+                if (isset($query_info)) {
+                    print_r($query_info);
+                }
+                ?>
+
+            </fieldset>
+            </br>
+            <fieldset>
+                <legend> Results:</legend>
+
+                <?php
+                    if (isset($curl_result)) {
+                        echo $curl_result;
+                    }
+                ?>
+            </fieldset>
+        </div>
+
+    </div>
+
+</body>
+</html>
